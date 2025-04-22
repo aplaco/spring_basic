8a854a59fe494bfe8b5da7d72e8e4ca3299e341f
aplaco <lyon108@naver.com>
2025-04-22 13:05:58 +0900

    test

--- src/main/java/com/example/basic/controller/JoinController.java
+++ src/main/java/com/example/basic/controller/JoinController.java
@@ -24,11 +24,8 @@ public class JoinController {
     }
 
     @PostMapping("/join/create")
-    public String create(@ModelAttribute JoinDTO formDTO, Model model  ){
-        String msg = joinService.processJoin(formDTO);
-        model.addAttribute("data", msg);
-        List<JoinEntity> users = joinService.getAllUsers();
-        System.out.println(users);
+    public String create( JoinDTO formDTO ){
+        joinService.processJoin(formDTO);
         return "redirect:/admin";
     }
 
@@ -39,10 +36,30 @@ public class JoinController {
         return "admin";
     }
 
+
     @GetMapping("/admin/del/{id}")
     public String delUser(@PathVariable Long id){
         joinService.delete(id);
         return "redirect:/admin";
     }
+
+    // 수정 버튼 클릭시 해당 메서드 어노테이션 호출
+    @GetMapping("/admin/edit/{id}")
+    public String editUser(@PathVariable Long id, Model model){
+        //서비스 메서드인 getUserById에 클릭한 게시글의 아이디값 전달해서 게시글 반환받음
+        JoinEntity user = joinService.getUserById(id);
+        //model.addAttribute 메서드에 의해서 edit 뷰화면에 해당 데이터 전달
+        model.addAttribute("user", user);
+        return "edit";
+    }
+
+    //수정폼 화면에서 최종 수정이 일어나는 컨트롤러
+    @PostMapping("/admin/update")
+    public String updateUser(JoinEntity formUser){
+        joinService.updateUser(formUser);
+        return "redirect:/admin";
+    }
+
+
 }
 
